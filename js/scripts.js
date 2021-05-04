19d70ae3a69d07d7aedf7a1449327f39e190ccf2
Lucian <lucian.miller185@gmail.com>
2021-05-04 10:35:06 -0700

    It will add variables for each qestion answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
+    const interestAnswer = parseInt($("input:radio[name=interest]:checked").val());
+    const difficultyAnswer = parseInt($("input:radio[name=difficulty]:checked").val());
+    const experienceAnswer = parseInt($("input:radio[name=experience]:checked").val());
+    const foodAnswer = parseInt($("input:radio[name=food]:checked").val());
+    const colorAnswer = parseInt($("input:radio[name=color]:checked").val());
+
     
     event.preventDefault();
   });
